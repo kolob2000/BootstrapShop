72aedd773abed0c8b9649ff6d563d29485b4520f
Nick <k.kolabis@mail.ru>
2022-02-04 09:44:59 +0300

    edits

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,4 +5,6 @@ app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp.main, name='index'),
+    path('product/', mainapp.ProductListView.as_view(), kwargs={'slug': None}, name='product'),
+    path('product/<slug:slug>/', mainapp.ProductListView.as_view(), name='category'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Product, Category, Instagram
 
 
@@ -11,3 +12,24 @@ def main(request):
         'insta_images': Instagram.objects.all().filter(is_active=True).order_by('?'),
     }
     return render(request, 'mainapp/index.html', context=context)
+
+
+class ProductListView(ListView):
+    model = Product
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        context['title'] = 'Продукты' if self.kwargs['slug'] is None else Category.objects.get(
+            slug=self.kwargs['slug']).title
+        context['categories'] = Category.objects.all().filter(is_active=True)
+        return context
+
+    def get_queryset(self):
+
+        query = Product.objects.all().filter(is_active=True) if self.kwargs[
+                                                                    'slug'] is None else Product.objects.all().filter(
+            category__slug=self.kwargs['slug'], is_active=True)
+        if self.request.GET.get('price') is None or str(self.request.GET.get('price')).isalpha():
+            return query
+        else:
+            return query.order_by('price' if int(self.request.GET.get('price')) == 1 else '-price')
